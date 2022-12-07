657e3701bb75ec80b84735abc106702f11a3e8bc
Vivek Jilla <vivekjilla7@gmail.com>
2022-12-07 11:59:38 +0530

    Update __init__.py

--- api/GetMessage/__init__.py
+++ api/GetMessage/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Python HTTP triggers function processed a request.")
+    logging.info("Python HTTP trigger function processed a request.")
     logging.info(f"{Path.cwd()}")
     message = Path("GetMessage/content.txt").read_text()
     return func.HttpResponse(message, status_code=200)
